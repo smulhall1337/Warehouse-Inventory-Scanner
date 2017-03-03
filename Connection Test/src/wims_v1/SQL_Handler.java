7ebb56c18cebc6eb7dcb545eeed7d17afbd58a0a
sprattj5 <jspratt.cs@gmail.com>
2017-03-02 20:07:11 -0500

    Updated file path

--- Connection Test/src/wims_v1/SQL_Handler.java
+++ Connection Test/src/wims_v1/SQL_Handler.java
@@ -1,28 +1,34 @@
+package wims_v1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class TestConnection {
-
-	public static void main(String[] args) throws Exception {
-		
-		getConnection();
-	}
-
+public class SQL_Handler {
 	
+	private String sql_driver;
+	private String url;
+	private String username;
+	private String password;
+	
+	public SQL_Handler() {
+		sql_driver = null;
+		url = null;
+		username = null;
+		password = null;
+	}
 	/**
 	 * method used to connect to the database 
 	 * @return the connection of the database
 	 * @return null if the connection cannot be established
 	 * @throws Exception
 	 */
-	public static Connection getConnection() throws Exception {
+	public Connection getConnection() throws Exception {
 		
 		try {
-			String driver = "com.mysql.jdbc.Driver";
-			String url = "jdbc:mysql://swenggseanmulhall.cdyrbvongw5v.us-east-1.rds.amazonaws.com:3306/swenggdb";
-			String username = "seansgroup";
-			String password = "Sssh.It'sasecret";
-			Class.forName(driver);
+			sql_driver = "com.mysql.jdbc.Driver";
+			url = "jdbc:mysql://swenggseanmulhall.cdyrbvongw5v.us-east-1.rds.amazonaws.com:3306/swenggdb";
+			username = "seansgroup";
+			password = "Sssh.It'sasecret";
+			Class.forName(sql_driver);
 			
 			Connection conn = DriverManager.getConnection(url, username, password);
 			System.out.println("Connected!");
@@ -50,6 +56,4 @@ public class TestConnection {
 	{
 		
 	}//edit end
-	
-	
-}//class end
+}
